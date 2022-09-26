bb52e15f03efe38798474e8f2ad7ff9e4a412ea9
sriya-nukala <sriyasetty13@gmail.com>
2022-09-26 22:03:52 +0530

    Fourth commit - user screen

--- app/src/main/java/com/example/draft_2/Signup_screen.java
+++ app/src/main/java/com/example/draft_2/Signup_screen.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Signup_screen extends AppCompatActivity {
     TextView t1;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
         t1=findViewById(R.id.login);
+        b1=findViewById(R.id.signupbtn);
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 login();
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1=new Intent(getApplicationContext(),option_screen.class);
+                startActivity(intent1);
             }
         });
 
